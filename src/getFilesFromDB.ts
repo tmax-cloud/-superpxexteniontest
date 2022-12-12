2909e4f029b6387e402f1be80445fd99719c8e70
ccsforever <48277126+ccsforever@users.noreply.github.com>
2022-12-12 16:40:59 +0900

    임시값 주서 처리

--- src/getFilesFromDB.ts
+++ src/getFilesFromDB.ts
@@ -60,121 +60,121 @@ export const getFilesFromDB = (memFs: MemFS, serverUrl: string, projectId: numbe
         });
     };
 
-    const tempProjectReq = {        
-        body: {
-          path: "ListSrcService",
-          statusCode: 200,
-          message: "success",
-          data: [
-            {
-              id: 1,
-              path: "src/test",
-              projectId: 1
-            },
-            {
-                id: 2,
-                path: "src/test2",
-                projectId: 1
-            },
-            {
-                id: 3,
-                path: "src/temp/test3",
-                projectId: 1
-            }
-          ]
-        }
-      };
-    const tempFileReq = [
-        {            
-            body: {
-              path: "DetailSrcService",
-              statusCode: 200,
-              message: "success",
-              data: [
-                {
-                  id: 1,
-                  number: 1,
-                  content: "hello",
-                  prevLineId: 0
-                },
-                {
-                  id: 2,
-                  number: 2,
-                  content: "world",
-                  prevLineId: 1
-                }
-              ]
-            }
-          },
-          {            
-            body: {
-              path: "DetailSrcService",
-              statusCode: 200,
-              message: "success",
-              data: [
-                {
-                  id: 1,
-                  number: 1,
-                  content: "hello2",
-                  prevLineId: 0
-                },
-                {
-                  id: 2,
-                  number: 2,
-                  content: "world2",
-                  prevLineId: 1
-                }
-              ]
-            }
-          },
-          {            
-            body: {
-              path: "DetailSrcService",
-              statusCode: 200,
-              message: "success",
-              data: [
-                {
-                  id: 1,
-                  number: 1,
-                  content: "hello3",
-                  prevLineId: 0
-                },
-                {
-                  id: 2,
-                  number: 2,
-                  content: "world3",
-                  prevLineId: 1
-                }
-              ]
-            }
-          }
+    // const tempProjectReq = {        
+    //     body: {
+    //       path: "ListSrcService",
+    //       statusCode: 200,
+    //       message: "success",
+    //       data: [
+    //         {
+    //           id: 1,
+    //           path: "src/test",
+    //           projectId: 1
+    //         },
+    //         {
+    //             id: 2,
+    //             path: "src/test2",
+    //             projectId: 1
+    //         },
+    //         {
+    //             id: 3,
+    //             path: "src/temp/test3",
+    //             projectId: 1
+    //         }
+    //       ]
+    //     }
+    //   };
+    // const tempFileReq = [
+    //     {            
+    //         body: {
+    //           path: "DetailSrcService",
+    //           statusCode: 200,
+    //           message: "success",
+    //           data: [
+    //             {
+    //               id: 1,
+    //               number: 1,
+    //               content: "hello",
+    //               prevLineId: 0
+    //             },
+    //             {
+    //               id: 2,
+    //               number: 2,
+    //               content: "world",
+    //               prevLineId: 1
+    //             }
+    //           ]
+    //         }
+    //       },
+    //       {            
+    //         body: {
+    //           path: "DetailSrcService",
+    //           statusCode: 200,
+    //           message: "success",
+    //           data: [
+    //             {
+    //               id: 1,
+    //               number: 1,
+    //               content: "hello2",
+    //               prevLineId: 0
+    //             },
+    //             {
+    //               id: 2,
+    //               number: 2,
+    //               content: "world2",
+    //               prevLineId: 1
+    //             }
+    //           ]
+    //         }
+    //       },
+    //       {            
+    //         body: {
+    //           path: "DetailSrcService",
+    //           statusCode: 200,
+    //           message: "success",
+    //           data: [
+    //             {
+    //               id: 1,
+    //               number: 1,
+    //               content: "hello3",
+    //               prevLineId: 0
+    //             },
+    //             {
+    //               id: 2,
+    //               number: 2,
+    //               content: "world3",
+    //               prevLineId: 1
+    //             }
+    //           ]
+    //         }
+    //       }
           
-    ];
+    // ];
       
-    tempProjectReq.body.data.forEach(d=>{
-        const lineData: string [] = [];
-        tempFileReq[d.id-1].body.data.forEach((d: any) => {
-            lineData.push(d.content);
-          });
-        const directory = d.path.split('/');
-        directory.pop();
+    // tempProjectReq.body.data.forEach(d=>{
+    //     const lineData: string [] = [];
+    //     tempFileReq[d.id-1].body.data.forEach((d: any) => {
+    //         lineData.push(d.content);
+    //       });
+    //     const directory = d.path.split('/');
+    //     directory.pop();
         
         
-        let temp = '/';        
-        directory.forEach((path)=>{
-            temp = temp + path + '/';
-            let existDirectory = false;
-            try {
-                memFs.readDirectory(vscode.Uri.parse(`dsfs:${temp}`));
-                existDirectory = true;
-            } catch {
-                existDirectory = false;
-            }            
-            !existDirectory && memFs.createDirectory(vscode.Uri.parse(`dsfs:${temp}`));
-        });
+    //     let temp = '/';        
+    //     directory.forEach((path)=>{
+    //         temp = temp + path + '/';
+    //         let existDirectory = false;
+    //         try {
+    //             memFs.readDirectory(vscode.Uri.parse(`dsfs:${temp}`));
+    //             existDirectory = true;
+    //         } catch {
+    //             existDirectory = false;
+    //         }            
+    //         !existDirectory && memFs.createDirectory(vscode.Uri.parse(`dsfs:${temp}`));
+    //     });
         
-        memFs.writeFile(vscode.Uri.parse(`dbfs:/${d.path}`), Buffer.from(lineData.join("")), { create: true, overwrite: true });
-    });
+    //     memFs.writeFile(vscode.Uri.parse(`dbfs:/${d.path}`), Buffer.from(lineData.join("")), { create: true, overwrite: true });
+    // });
       
     
 };
